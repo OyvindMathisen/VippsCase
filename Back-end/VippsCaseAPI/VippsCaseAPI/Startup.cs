562661e1bcf3798ec8f496802e24c4b7011123ed
Fredrik Svendsen <fredrik.n.svendsen@gmail.com>
2019-10-09 10:23:43 +0200

    Feat(DBContext): Added dbcontext

--- Back-end/VippsCaseAPI/VippsCaseAPI/Startup.cs
+++ Back-end/VippsCaseAPI/VippsCaseAPI/Startup.cs
@@ -6,10 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using VippsCaseAPI.DataAccess;
+using static Microsoft.AspNetCore.Hosting.Internal.HostingApplication;
 
 namespace VippsCaseAPI
 {
@@ -25,6 +28,7 @@ namespace VippsCaseAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DBContext>(options => options.UseSqlServer(Configuration["ConnectionString:VippsCaseDev"]));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
