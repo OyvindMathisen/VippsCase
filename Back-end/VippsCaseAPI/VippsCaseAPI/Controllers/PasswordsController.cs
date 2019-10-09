cbaa3ffa23c58869729adf01fad2a16b68fe41a1
Sindre <sindrefuru@hotmail.no>
2019-10-09 11:36:29 +0200

    feat: Added Controllers

---
+++ Back-end/VippsCaseAPI/VippsCaseAPI/Controllers/PasswordsController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VippsCaseAPI.DataAccess;
+using VippsCaseAPI.Models;
+
+namespace VippsCaseAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PasswordsController : ControllerBase
+    {
+        private readonly DBContext _context;
+
+        public PasswordsController(DBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Passwords
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Password>>> Getpasswords()
+        {
+            return await _context.passwords.ToListAsync();
+        }
+
+        // GET: api/Passwords/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Password>> GetPassword(int id)
+        {
+            var password = await _context.passwords.FindAsync(id);
+
+            if (password == null)
+            {
+                return NotFound();
+            }
+
+            return password;
+        }
+
+        // PUT: api/Passwords/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPassword(int id, Password password)
+        {
+            if (id != password.PasswordId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(password).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PasswordExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Passwords
+        [HttpPost]
+        public async Task<ActionResult<Password>> PostPassword(Password password)
+        {
+            _context.passwords.Add(password);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPassword", new { id = password.PasswordId }, password);
+        }
+
+        // DELETE: api/Passwords/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Password>> DeletePassword(int id)
+        {
+            var password = await _context.passwords.FindAsync(id);
+            if (password == null)
+            {
+                return NotFound();
+            }
+
+            _context.passwords.Remove(password);
+            await _context.SaveChangesAsync();
+
+            return password;
+        }
+
+        private bool PasswordExists(int id)
+        {
+            return _context.passwords.Any(e => e.PasswordId == id);
+        }
+    }
+}
