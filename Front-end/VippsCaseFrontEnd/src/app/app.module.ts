736438e373320a958a1cf568318d2bc4fa1fd4f0
Eddie Kusturica <eddkus@outlook.com>
2019-10-10 13:39:46 +0200

    Chore(): Initialized all pages

--- Front-end/VippsCaseFrontEnd/src/app/app.module.ts
+++ Front-end/VippsCaseFrontEnd/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './views/login-form/login-form.component';
+import { PurchasePageComponent } from './views/purchase-page/purchase-page.component';
+import { PurchaseHistoryComponent } from './views/purchase-history/purchase-history.component';
+import { PurchaseConfirmedComponent } from './views/purchase-confirmed/purchase-confirmed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    PurchasePageComponent,
+    PurchaseHistoryComponent,
+    PurchaseConfirmedComponent
   ],
   imports: [
     BrowserModule,
