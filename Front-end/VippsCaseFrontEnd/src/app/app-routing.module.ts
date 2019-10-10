179a71b61c3010ddccd9711ef68adfdf0435eb15
Sindre <sindrefuru@hotmail.no>
2019-10-10 14:21:38 +0200

    Chore(app-routing.module): Added router to all pages

--- Front-end/VippsCaseFrontEnd/src/app/app-routing.module.ts
+++ Front-end/VippsCaseFrontEnd/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginFormComponent } from './views/login-form/login-form.component';
+import { PurchasePageComponent } from './views/purchase-page/purchase-page.component';
+import { PurchaseHistoryComponent } from './views/purchase-history/purchase-history.component';
+import { PurchaseConfirmedComponent } from './views/purchase-confirmed/purchase-confirmed.component';
 
 
 const routes: Routes = [
-  { path: "", component: LoginFormComponent }
+  { path: "", component: LoginFormComponent },
+  { path: "/purchase", component: PurchasePageComponent },
+  { path: "/confirmation", component: PurchaseConfirmedComponent },
+  { path: "/profile", component: PurchaseHistoryComponent }
 ];
 
 @NgModule({
