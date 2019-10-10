e2a51ef09ee72aa37cb7213d6e6079b404c04e42
Eddie Kusturica <eddkus@outlook.com>
2019-10-10 14:12:43 +0200

    Chore: Testing route git conflict

--- Front-end/VippsCaseFrontEnd/src/app/app-routing.module.ts
+++ Front-end/VippsCaseFrontEnd/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PurchasePageComponent } from './views/purchase-page/purchase-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "/shopping-cart", component: PurchasePageComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
