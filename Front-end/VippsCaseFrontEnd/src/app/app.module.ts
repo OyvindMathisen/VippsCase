468937aa4a09cd996e46c297a7187f5e62a9b2fb
Eddie Kusturica <eddkus@outlook.com>
2019-10-10 13:52:23 +0200

    Chore: Changed name of some components

--- Front-end/VippsCaseFrontEnd/src/app/app.module.ts
+++ Front-end/VippsCaseFrontEnd/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { LoginFormComponent } from './views/login-form/login-form.component';
 import { PurchasePageComponent } from './views/purchase-page/purchase-page.component';
 import { PurchaseHistoryComponent } from './views/purchase-history/purchase-history.component';
 import { PurchaseConfirmedComponent } from './views/purchase-confirmed/purchase-confirmed.component';
+import { ItemListComponent } from './components/item-list/item-list.component';
+import { ItemComponent } from './components/item/item.component';
+import { BillComponent } from './components/bill/bill.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { PurchaseConfirmedComponent } from './views/purchase-confirmed/purchase-
     LoginFormComponent,
     PurchasePageComponent,
     PurchaseHistoryComponent,
-    PurchaseConfirmedComponent
+    PurchaseConfirmedComponent,
+    ItemListComponent,
+    ItemComponent,
+    BillComponent
   ],
   imports: [
     BrowserModule,
