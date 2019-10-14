dd97f75718aae9e6c8623c25af7c4ba06193433d
Fredrik Svendsen <fredrik.n.svendsen@gmail.com>
2019-10-14 08:10:56 +0200

    Chore: Added web deploy settings and gitignore

--- Back-end/VippsCaseAPI/VippsCaseAPI/Startup.cs
+++ Back-end/VippsCaseAPI/VippsCaseAPI/Startup.cs
@@ -42,7 +42,8 @@ namespace VippsCaseAPI
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
+                app.UseDeveloperExceptionPage();
             }
 
             app.UseHttpsRedirection();
