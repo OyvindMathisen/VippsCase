7cc30c38f8d61eba6af66efe7e038d16a3002806
Øyvind Mathisen <OyvindMathisenWork@gmail.com>
2019-10-15 11:06:46 +0200

    Chore: Created the stripe card input component.

---
+++ Front-end/VippsCaseFrontEnd/src/app/components/stripe-card-input/stripe-card-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stripe-card-input',
+  templateUrl: './stripe-card-input.component.html',
+  styleUrls: ['./stripe-card-input.component.scss']
+})
+export class StripeCardInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
