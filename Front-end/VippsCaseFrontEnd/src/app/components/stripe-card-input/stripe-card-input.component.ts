bf2cd642be3ac322b64d7d75e7f57789cce6a25a
Øyvind Mathisen <OyvindMathisenWork@gmail.com>
2019-10-15 12:05:12 +0200

    Feat: Added the card input field with error handling

--- Front-end/VippsCaseFrontEnd/src/app/components/stripe-card-input/stripe-card-input.component.ts
+++ Front-end/VippsCaseFrontEnd/src/app/components/stripe-card-input/stripe-card-input.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { StripeService } from '../../services/stripe.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-stripe-card-input',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stripe-card-input.component.scss']
 })
 export class StripeCardInputComponent implements OnInit {
+  private card: stripe.elements.Element;
 
-  constructor() { }
+  constructor(private stripeService: StripeService) {
+    // Stripe content to let us create the input
+    const stripe = Stripe(environment.stripeKey);
+    const elements = stripe.elements();
+    this.card = elements.create('card', {hidePostalCode: true});
+
+    // Display error handling to the user
+    this.card.on('change', (event) => {
+      const displayError = document.getElementById('card-errors');
+      if (event.error) {
+        displayError.textContent = event.error.message;
+      } else {
+        displayError.textContent = '';
+      }
+    });
+  }
 
   ngOnInit() {
+    // Add the card to the component
+    this.card.mount('#card-element');
   }
 
 }
