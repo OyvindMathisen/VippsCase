75ab42b4e8dc3744e32aad310b948ecfa72b1024
Eddie Kusturica <eddkus@outlook.com>
2019-10-15 14:06:26 +0200

    Feat: Fixed Login Authentication

--- Front-end/VippsCaseFrontEnd/src/app/components/login-module/login-module.component.ts
+++ Front-end/VippsCaseFrontEnd/src/app/components/login-module/login-module.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { LoginService } from "../../services/login.service";
+import { Login } from "../../shared/models/login.model";
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login-module',
@@ -11,9 +15,43 @@ import { HttpClient } from "@angular/common/http";
 @Injectable()
 export class LoginModuleComponent implements OnInit {
 
-  constructor(private http: HttpClient) { }
+  @Output() errorMessage: string;
+
+  login = {} as Login;
+  loginError = false; 
+
+  constructor(private http: HttpClient,
+    private loginService: LoginService,
+    private router: Router) { }
 
   ngOnInit() {
   }
-  
+
+  loginClicked(email, pwd){
+    console.log(email);
+    console.log(pwd);
+    
+    this.login = {
+      email: email,
+      password: pwd
+    }
+    
+    
+    this.loginService.login(this.login).subscribe(
+      data => {
+        this.router.navigate(['/purchase']);
+        console.log(data);
+        this.loginError = false;
+      },
+      error => {
+        console.log(error);
+        this.loginError = true;
+        this.errorMessage = error.statusText + ": Invalid username or password!";
+        
+      }
+    );
+
+    // Returning false to prevent the page from updating
+    return false;
+  }
 }
