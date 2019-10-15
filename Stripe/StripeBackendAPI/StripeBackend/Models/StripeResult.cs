f82c6de91a2aa6cf527157e455480e0c55978873
Øyvind Mathisen <OyvindMathisenWork@gmail.com>
2019-10-15 15:37:45 +0200

    Chore: Updated file names to better reflect their uses

--- Stripe/StripeBackendAPI/StripeBackend/Models/StripeResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StripeBackend.Models
-{
-    public class StripeResult
-    {
-        public bool Successful { get; set; }
-        public string ResultToken { get; set; }
-        public string ErrorMessage { get; set; }
-    }
-}
