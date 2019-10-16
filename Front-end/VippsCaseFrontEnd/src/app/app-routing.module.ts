f412d7a7dcf0aff366475d951c42eb230a15cacc
Øyvind Mathisen <OyvindMathisenWork@gmail.com>
2019-10-16 14:46:07 +0200

    Chore: Changing " to ' in the routing file.

--- Front-end/VippsCaseFrontEnd/src/app/app-routing.module.ts
+++ Front-end/VippsCaseFrontEnd/src/app/app-routing.module.ts
@@ -7,10 +7,10 @@ import { PurchaseConfirmedComponent } from './views/purchase-confirmed/purchase-
 
 
 const routes: Routes = [
-  { path: "", component: LoginFormComponent },
-  { path: "purchase", component: PurchasePageComponent },
-  { path: "confirmation", component: PurchaseConfirmedComponent },
-  { path: "profile", component: PurchaseHistoryComponent }
+  { path: '', component: LoginFormComponent },
+  { path: 'purchase', component: PurchasePageComponent },
+  { path: 'confirmation', component: PurchaseConfirmedComponent },
+  { path: 'profile', component: PurchaseHistoryComponent }
 ];
 
 @NgModule({
