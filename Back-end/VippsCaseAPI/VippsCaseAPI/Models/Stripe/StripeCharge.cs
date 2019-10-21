4a0ceeda652c74f8d86a84251beea0b58db8e7a7
Øyvind Mathisen <OyvindMathisenWork@gmail.com>
2019-10-21 13:42:26 +0200

    Feat: Adding customer to order based on cart id and user id

--- Back-end/VippsCaseAPI/VippsCaseAPI/Models/Stripe/StripeCharge.cs
+++ Back-end/VippsCaseAPI/VippsCaseAPI/Models/Stripe/StripeCharge.cs
@@ -4,13 +4,14 @@
     {
         public string StripeToken { get; set; }
         public long TotalCost { get; set; }
-        public string IdempotencyToken { get; set; }
+        public int UserId { get; set; }
+        public int CartId { get; set; }
 
         public StripeCustomer CustomerDetails { get; set; }
 
         public override string ToString()
         {
-            return $"StripeToken: {StripeToken}, TotalCost: {TotalCost}, CustomerDetails: {CustomerDetails}, IdempotencyToken: {IdempotencyToken}";
+            return $"StripeToken: {StripeToken}, TotalCost: {TotalCost}, CustomerDetails: {CustomerDetails}, UserId: {UserId}, CartId: {CartId}.";
         }
     }
 }
